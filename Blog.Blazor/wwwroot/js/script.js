a70db5439615b7eb77f1d612f141c7eb77d437f3
nuc <livebug@163.com>
2021-10-29 23:18:50 +0800

    处理锚点导航问题： 1 锚点不起作用 导航使用绝对路径+#锚点 2 锚点滚动之后被顶部导航遮盖，padding 影响锚点元素的定位，margin 不影响锚点元素的定位。所以使用 padding 调整锚点元素跳转后的位置，再使用 margin 抵消 padding 对布局的影响。

--- Blog.Blazor/wwwroot/js/script.js
+++ Blog.Blazor/wwwroot/js/script.js
@@ -1,3 +1,36 @@
-﻿export function myfunction(message) {
-    console.log(message);
+﻿export function myfunction() {
+    let markdown = document.getElementById('blog-markdown-body');
+    for (let i = 0; i < markdown.childElementCount; i++) {
+        const element = markdown.children[i];
+        switch (element.tagName) {
+            case 'H1':
+                printTitle(1, element);
+                break;
+            case 'H2':
+                printTitle(2, element);
+                break;
+            case 'H3':
+                printTitle(3, element);
+                break;
+            case 'H4':
+                printTitle(4, element);
+                break;
+            case 'H5':
+                printTitle(5, element);
+                break;
+            case 'H6':
+                printTitle(6, element);
+                break;
+            default:
+                break;
+        }
+    }
+}
+
+function printTitle(headerTag, element) {
+    let toc = document.getElementById('blog-markdown-toc');
+    toc.setAttribute('style', 'font-size:0.5em;');
+    let root = window.location.href.replace(window.location.hash,''); 
+    toc.innerHTML += '<li><a style=\'padding-left: ' + (headerTag - 1) + 'rem;\' href=\'' + root + '#' + element.getAttribute('id') + '\'  target=\'_top\' >' + element.innerHTML + ' ' + '</a></li>';
+    console.log(headerTag + ' ' + element.getAttribute('id'));
 }
