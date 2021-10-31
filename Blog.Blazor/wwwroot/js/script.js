d4a8bdeed2b569712accbc615d9c0c88350c7730
sharif <3188208752@qq.com>
2021-11-01 01:31:33 +0800

    20211101 完成文档主题黑白切换（只支持文档，暂时不包括整个网站）；增加功能板可以陆续添加小功能

--- Blog.Blazor/wwwroot/js/script.js
+++ Blog.Blazor/wwwroot/js/script.js
@@ -14,3 +14,37 @@
         }
     }
 }
+
+const light = 'css/github-markdown-css/github-markdown-light.css';
+const dark = 'css/github-markdown-css/github-markdown-dark.css';
+export function switchTheme(themeId) {
+    let style = document.getElementById(themeId);
+    let theme = localStorage.getItem(themeId);
+    if (theme == "light") {
+        style.href = dark;
+        localStorage.setItem(themeId, "dark");
+    }
+    else if (theme == "dark") {
+        style.href = light;
+        localStorage.setItem(themeId, "light");
+    }
+}
+
+export function getMarkdownTheme(themeId) {
+    let style = document.getElementById(themeId);
+    let theme = localStorage.getItem(themeId);
+    console.log('js ' + theme);
+    if (theme == "light") {
+        style.href = light;
+        return "light";
+    }
+    else if (theme == "dark") {
+        style.href = dark;
+        return "dark";
+    }
+    else {
+        localStorage.setItem(themeId, "light");
+        style.href = dark;
+        return "light";
+    }
+}
