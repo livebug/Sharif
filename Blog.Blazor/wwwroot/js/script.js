83e1751a96575d5f60109b36684acde4922c89e3
sharif <3188208752@qq.com>
2021-10-31 13:09:09 +0800

    20211031 完成文章目录，实现目录根据文章html自动生成。

--- Blog.Blazor/wwwroot/js/script.js
+++ Blog.Blazor/wwwroot/js/script.js
@@ -1,36 +1,16 @@
-﻿export function myfunction() {
-    let markdown = document.getElementById('blog-markdown-body');
+﻿export function generateTOC(bodyId, tocId) {
+    // hrefBase 生成toc链接基础 根地址+#
+    let hrefBase = window.location.href.replace(window.location.hash, '') + '#';
+    // 获取 markdown 的文章内容与目录节点
+    let markdown = document.getElementById(bodyId);
+    let toc = document.getElementById(tocId);
+    // 遍历 markdown 文章html 提取目录 并生成目录
     for (let i = 0; i < markdown.childElementCount; i++) {
         const element = markdown.children[i];
-        switch (element.tagName) {
-            case 'H1':
-                printTitle(1, element);
-                break;
-            case 'H2':
-                printTitle(2, element);
-                break;
-            case 'H3':
-                printTitle(3, element);
-                break;
-            case 'H4':
-                printTitle(4, element);
-                break;
-            case 'H5':
-                printTitle(5, element);
-                break;
-            case 'H6':
-                printTitle(6, element);
-                break;
-            default:
-                break;
+        if (element.tagName[0] === 'H') {
+            toc.innerHTML += `<li  style=\'padding-left:${(element.tagName[1] - 1) * 0.5}rem;\'>
+                                  <a href=\'${hrefBase}${element.getAttribute('id')}\'  target=\'_top\' >${element.innerHTML}</a>
+                              </li>`;
         }
     }
-}
-
-function printTitle(headerTag, element) {
-    let toc = document.getElementById('blog-markdown-toc');
-    toc.setAttribute('style', 'font-size:0.5em;');
-    let root = window.location.href.replace(window.location.hash,''); 
-    toc.innerHTML += '<li  style=\'padding-left: ' + (headerTag - 1)*0.5 + 'rem;\'><a href=\'' + root + '#' + element.getAttribute('id') + '\'  target=\'_top\' >' + element.innerHTML + ' ' + '</a></li>';
-    console.log(headerTag + ' ' + element.getAttribute('id'));
 }
