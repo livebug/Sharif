1cb407ac7d9fb68f49e4d9dd6b1e47f8211c3644
sharif <3188208752@qq.com>
2021-11-16 18:37:21 +0800

    20211116 新增HiveParser测试，调试HiveParser，测试通过，并打印LISP风格分析树

---
+++ Blog.Test/SQLC/HiveParserTests.cs
@@ -0,0 +1,28 @@
+﻿using Antlr4.Runtime;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestClass()]
+    public class HiveParserTests
+    {
+        [TestMethod()]
+        public void HiveParserTest()
+        {
+            string sql = "WITH A AS (SELECT * FROM TB) SELECT A,B,C FROM A";
+            var charStream = CharStreams.fromString(sql);
+            var lexer = new HiveLexer(charStream);
+            var tokens = new CommonTokenStream(lexer);
+            var parser = new HiveParser(tokens);
+            parser.BuildParseTree = true;
+            var tree = parser.statements();
+            Console.WriteLine(tree.ToStringTree());
+            Assert.IsNotNull(tree);
+        }
+    }
+}
